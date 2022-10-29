ccb853debd339a041671ae0700f0324b08ed3151
Pratham <pratham.sharma2105@gmail.com>
2022-10-29 21:26:25 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,8 @@ export default function Home() {
           "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(1).png?alt=media&token=8963b61f-bf65-4c95-b5ef-f9be13584397"
         }
         placeholder="blur"
-        fill
+        width={230}
+        height={100}
       />
       <Image
         src="https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(2).png?alt=media&token=23fa4b19-2144-4171-9b8a-dc696ccf2327"
@@ -19,7 +20,8 @@ export default function Home() {
           "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(2).png?alt=media&token=23fa4b19-2144-4171-9b8a-dc696ccf2327"
         }
         placeholder="blur"
-        fill
+        width={230}
+        height={100}
       />
       <Image
         src="https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(3).png?alt=media&token=96fdfc5b-9ae0-4800-a890-ec8e0ef241d7"
@@ -28,7 +30,8 @@ export default function Home() {
           "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(3).png?alt=media&token=96fdfc5b-9ae0-4800-a890-ec8e0ef241d7"
         }
         placeholder="blur"
-        fill
+        width={230}
+        height={100}
       />
       <Image
         src="https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(4).png?alt=media&token=9e5e97cd-d2eb-4aa4-8f4e-91027014b904"
@@ -37,7 +40,8 @@ export default function Home() {
           "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(4).png?alt=media&token=9e5e97cd-d2eb-4aa4-8f4e-91027014b904"
         }
         placeholder="blur"
-        fill
+        width={230}
+        height={100}
       />
     </div>
   );
