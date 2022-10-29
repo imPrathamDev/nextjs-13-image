c44a34215033873b09519f1aaabe234e86e37683
Pratham <pratham.sharma2105@gmail.com>
2022-10-29 21:04:05 +0530

    next/image

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
