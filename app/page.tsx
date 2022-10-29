43cd07e087463d271edf118a51b2e4633ddafa99
Pratham <pratham.sharma2105@gmail.com>
2022-10-29 21:23:40 +0530

    images added

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,33 @@ export default function Home() {
         placeholder="blur"
         fill
       />
+      <Image
+        src="https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(2).png?alt=media&token=23fa4b19-2144-4171-9b8a-dc696ccf2327"
+        alt="Vercel Logo"
+        blurDataURL={
+          "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(2).png?alt=media&token=23fa4b19-2144-4171-9b8a-dc696ccf2327"
+        }
+        placeholder="blur"
+        fill
+      />
+      <Image
+        src="https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(3).png?alt=media&token=96fdfc5b-9ae0-4800-a890-ec8e0ef241d7"
+        alt="Vercel Logo"
+        blurDataURL={
+          "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(3).png?alt=media&token=96fdfc5b-9ae0-4800-a890-ec8e0ef241d7"
+        }
+        placeholder="blur"
+        fill
+      />
+      <Image
+        src="https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(4).png?alt=media&token=9e5e97cd-d2eb-4aa4-8f4e-91027014b904"
+        alt="Vercel Logo"
+        blurDataURL={
+          "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(4).png?alt=media&token=9e5e97cd-d2eb-4aa4-8f4e-91027014b904"
+        }
+        placeholder="blur"
+        fill
+      />
     </div>
   );
 }
